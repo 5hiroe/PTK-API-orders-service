471b2dfca0390817994eb41b67fe135b256d6ef6
シロエ <n.caruelle@icloud.com>
2024-09-15 14:04:08 +0200

    Update validator

--- src/validators/model.js
+++ src/validators/model.js
@@ -3,9 +3,9 @@ import Joi from 'joi'
 export const order = Joi.object({
     date: Joi.date().required(),
     status: Joi.string().max(100).required(),
-    totalAmount: Joi.string().max(10).required(),
+    totalAmount: Joi.number().required(),
     items: Joi.array().items(Joi.object({
         product_id: Joi.string().max(100).required(),
-        quantity: Joi.string().max(100).required(),
+        quantity: Joi.number().required(),
     })).required()
 })
